0cc880f7483afdd9773f3a215c391bf96766b8f9
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-05-15 09:00:49 -0300

    FIX: Change users to user

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const User = sequelize.define('users', {
+const User = sequelize.define('user', {
   id: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
