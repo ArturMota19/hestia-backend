7c2fd48d91e8847b7242e3b53cb42e0da0fd7c99
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-07-03 11:38:15 -0300

    WIP: Add edit to activity

--- controllers/activitiesPresetParamController.js
+++ controllers/activitiesPresetParamController.js
@@ -121,7 +121,7 @@ exports.deleteById = async (req, res) => {
     if(routineActivitiesReference){
       return res.status(423).json({ message: "Cannot delete: referenced elsewhere" });
     }
-    
+
     await ActuatorsActivity.destroy({ where: { activityPresetParamId: id } });
     await OtherActivities.destroy({ where: { activityPresetParamId: id } });
     await activitiesPresetParam.destroy({ where: { id, userId } });
@@ -129,5 +129,82 @@ exports.deleteById = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateById = async (req, res) => {
+  const { id } = req.params;
+  const { activity, actuators, otherActivities, presetId, room, name } = req.body;
+  const userId = req.users.id;
+
+  if (!activity || !name || !presetId || !room) {
+    return res.status(400).json({ error: 'Insufficient data to update routine' });
+  }
+
+  const transaction = await ActivityPresetParam.sequelize.transaction();
+  try {
+    const activityPresetParam = await ActivityPresetParam.findOne({ where: { id, userId } });
+    if (!activityPresetParam) {
+      await transaction.rollback();
+      return res.status(404).json({ error: 'Activity Preset Param not found' });
+    }
+
+    await activityPresetParam.update({
+      name,
+      presetId,
+      activityId: activity.id,
+      activityRoom: room.id
+    }, { transaction });
+
+    await ActuatorsActivity.destroy({ where: { activityPresetParamId: id }, transaction });
+    for (const item of actuators) {
+      const actuatorId = item.actuator.actuatorId;
+      const statusMap = Object.fromEntries(item.status.map(({ name, value }) => [name, value]));
+      const statusFields = [
+        'switch_led',
+        'bright_value_v2',
+        'temp_value_v2',
+        'switch',
+        'switch_1',
+        'sound_volume',
+        'temp_set',
+        'mode',
+        'presence_state',
+        'human_motion_state'
+      ];
+      const actuatorStatus = {};
+      for (const field of statusFields) {
+        if (statusMap[field] !== undefined) {
+          if (statusMap[field] == false) {
+            actuatorStatus[field] = "OFF";
+            continue;
+          }
+          actuatorStatus[field] = statusMap[field];
+        }
+      }
+      await ActuatorsActivity.create({
+        id: uuidv4(),
+        activityPresetParamId: id,
+        actuatorId,
+        ...actuatorStatus
+      }, { transaction });
+    }
+
+    await OtherActivities.destroy({ where: { activityPresetParamId: id }, transaction });
+    for (const item of otherActivities) {
+      await OtherActivities.create({
+        id: uuidv4(),
+        activityPresetParamId: id,
+        activityId: item.otherActivity.id,
+        probability: item.probability,
+      }, { transaction });
+    }
+
+    await transaction.commit();
+    return res.status(200).json({ message: 'Activity Preset param updated successfully!' });
+  } catch (err) {
+    await transaction.rollback();
+    console.error(err);
+    return res.status(500).json({ error: 'Error updating routine', details: err.message });
   }
 };
