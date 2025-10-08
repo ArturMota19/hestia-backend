63b8748b0292f685579ad36cb31cd8f79500ca2b
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-10-08 14:29:11 -0300

    Deploy force

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/peoplePriority", peoplePriority)
 
 const PORT = process.env.PORT || 3000;
 sequelize
-  .sync({ alter: false })
+  .sync({ force: true })
   .then(async () => {
     const existingUser = await Users.findOne({ where: { isAdmin: true } });
 
